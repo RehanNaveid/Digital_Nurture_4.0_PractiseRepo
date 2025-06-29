77066196f5bca8fc306f1ff6ee0bc8cd1642d4d8
Rehan Naveid <119671994+RehanNaveid@users.noreply.github.com>
2025-06-30 02:51:48 +0530

    1st commit

---
+++ Week_2/Moq/CustomerCommLib/CustomerCommLib.Tests/CustomerCommTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using Moq;
+using CustomerCommLib;
+
+namespace CustomerCommLib.Tests
+{
+    [TestFixture]
+    public class CustomerCommTests
+    {
+        [Test]
+        public void SendMailToCustomer_ShouldReturnTrue_WhenMailIsSentSuccessfully()
+        {
+            var mockMailSender = new Mock<IMailSender>();
+            mockMailSender
+                .Setup(m => m.SendMail(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(true);
+
+            var customerComm = new CustomerComm(mockMailSender.Object);
+
+            var result = customerComm.SendMailToCustomer();
+
+            Assert.That(result, Is.True);  // ✅ Alternative to Assert.IsTrue
+        }
+    }
+}
