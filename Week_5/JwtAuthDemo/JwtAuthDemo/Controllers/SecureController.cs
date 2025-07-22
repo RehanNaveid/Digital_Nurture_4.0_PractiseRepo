8d6ec5b74d82992b73702aa344a56c9bba8f3f02
Rehan Naveid <119671994+RehanNaveid@users.noreply.github.com>
2025-07-22 22:24:53 +0530

    first commit

---
+++ Week_5/JwtAuthDemo/JwtAuthDemo/Controllers/SecureController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAuthDemo.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SecureController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("This is a protected endpoint. You are authenticated.");
+        }
+    }
+}
